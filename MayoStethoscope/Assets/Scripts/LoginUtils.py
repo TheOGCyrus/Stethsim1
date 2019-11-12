9176f3f4c4cad2025d5d7599666b2723c1cc7c1f
Matthew Mick <cyrussilvermist@gmail.com>
2019-11-12 17:06:24 -0500

    login features

---
+++ MayoStethoscope/Assets/Scripts/LoginUtils.py
@@ -0,0 +1,35 @@
+database = {}
+
+def Start(username, password, action):
+    populate_database()
+
+    if "L" in action:
+        return Login(username, password)
+    elif "C" in action:
+        return Create(username, password)
+    elif "F" in action:
+        return Forgot(username)
+
+def Login(username, password):
+    if username in database.keys():
+        if password == database[password]:
+            return "True"
+    return "False"
+
+def Create(username, password):
+    if username not in database.keys():
+        file = open("database.txt", "a")
+        file.write(username + " " + password)
+        file.close()
+        return "True"
+    return "The username already exists."
+
+def Forgot(username):
+    return
+
+def populate_database():
+    global database
+    with open("database.txt") as file:
+        for line in file:
+            (key, val) = line.split()
+            database[int(key)] = val
